6aa43434377de682246d50324155ae6e655bb288
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-13 09:40:08 -0300

    fix: typo on file sizes

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -7,7 +7,7 @@ const formatBytes = (bytes, decimals = 2) => {
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["BYtes", "KB", "MB", "GB", "TB", "PB"];
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
