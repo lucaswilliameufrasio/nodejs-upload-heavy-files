16e71271152b7a4f24434a984ddc022e4e80ae62
Lucas William <lucaswilliameufrasio@gmail.com>
2022-02-06 19:30:13 -0300

    refactor: replace fiber with gin to use go-socket.io

--- backend-golang/src/main.go
+++ backend-golang/src/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
 )
 
-func healthcheck(c *fiber.Ctx) error {
-	return c.JSON(map[string]interface{}{
+func healthcheck(c *gin.Context) {
+	c.JSON(200, gin.H{
 		"message": "Hello, World 👋!",
 	})
 }
 
 func main() {
-	app := fiber.New()
+	router := gin.New()
 
-	app.Get("/healthcheck", healthcheck)
+	server := socketio.NewServer(nil)
 
-	app.Listen(":3000")
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		log.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		log.Println("meet error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println("closed", reason)
+	})
+
+	go func() {
+		if err := server.Serve(); err != nil {
+			log.Fatalf("socketio listen error: %s\n", err)
+		}
+	}()
+	defer server.Close()
+
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
+	router.GET("/healthcheck", healthcheck)
+
+	router.Run(":3000")
 }
